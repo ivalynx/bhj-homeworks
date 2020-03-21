4e2106b20b77f7c1cf33c29b1bfaf92d1a269d4d
Iva Kochneva <inkybasik@gmail.com>
2020-03-21 09:08:11 +0300

    Changed cycle

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -12,13 +12,10 @@ function moleGame() {
   };
 
   // вариант с циклом. Для меня совсем неочевидный :( 
-  // Поскольку в тз было указано "через цикл", я подумала, что обработчик события добавлять надо тоже через цикл.
-  // Но так не работает, и писать несколько строк с одним и тем же значением мне казалось настолько нерациональным,
-  // что я в процессе обдумывания этого решения сначала решила через делегирование (спасибо подсказке в тг-чате, т.к. я не знала этого слова)
 
     const hole_field = document.getElementsByClassName('hole');
-    for(let i = 0; i < hole_field.length; i++) {
-      hole_field[i].onclick = clickMole;
+    for (const hole of hole_field) {
+      hole.onclick = clickMole;
     }
     
     function clickMole() {
@@ -37,7 +34,7 @@ function moleGame() {
       lost.textContent = lostMole;
     }
 
-  // Вариант без цикла, через делегирование. Имхо, наиболее логичный вариант. 
+  // Вариант без цикла, через делегирование. Имхо, более логичный вариант. 
 
   // const holeField = document.getElementsByClassName('hole-game')[0];
 
