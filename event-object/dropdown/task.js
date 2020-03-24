4318a272961f7227f88a64e0caa158c233bcbf26
Iva Kochneva <inkybasik@gmail.com>
2020-03-24 23:07:49 +0300

    Dropdown task done

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function dropdown() {
+  const dropdown__values = document.querySelectorAll('.dropdown__value');
+  const dropdown__lists = document.querySelector('.dropdown').querySelectorAll('.dropdown__list')
+  for(let i = 0; i < dropdown__values.length; i++) {
+    dropdown__values[i].addEventListener('click', toggleDropdownList);
+  };
+  for(let i = 0; i < dropdown__lists.length; i++) {
+    dropdown__lists[i].addEventListener('click', changeButton);
+  };
+  function toggleDropdownList(event) {
+    const thisDropdownListClassList =  event.target.nextElementSibling.classList;
+    thisDropdownListClassList.toggle ('dropdown__list_active');
+  };
+  function changeButton(event) {  
+    event.target.closest('.dropdown__list').previousElementSibling.textContent = event.target.textContent;
+    event.preventDefault();
+  }
+};
+document.addEventListener("DOMContentLoaded", dropdown);
