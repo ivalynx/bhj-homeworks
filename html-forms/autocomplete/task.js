c5fb8fc658b41704432bb0b528715fdc140f30b7
Iva Kochneva <inkybasik@gmail.com>
2020-03-31 22:41:54 +0300

    Task autocomplete done

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,18 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    let result = [];
+    const options = this.input.getElementsByTagName('option');
+    
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].text.includes(text)) {
+        result.push({
+          text: options[i].text,
+          value: options[i].value
+        })
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    };
+    return result;
   }
 }
 
