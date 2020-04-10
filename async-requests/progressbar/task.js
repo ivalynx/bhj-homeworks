457d58aabec3eaeebc8b13087fcc16219de3e53f
Iva Kochneva <inkybasik@gmail.com>
2020-04-10 21:36:54 +0300

    I need help with xhr.upload

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,19 +1,23 @@
 'use strict'
 
 function poll() {
-  const progress = document.getElementById('progress');
   const form = document.getElementById('form');
-  const xhr = new XMLHttpRequest();
-  const formData = new FormData();
-  formData.append('file', 'file');
-  form.addEventListener('submit', event => {
+  form.addEventListener('submit', (event) => {   
+    const formData = new FormData(form); 
+    const progress = document.getElementById('progress');
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php', true);
     xhr.upload.onprogress = function() {
+      // alert('Загружено на сервер');
+      progress.value = 0.5;
+    }
+    xhr.upload.onload = function() {
       progress.value = 1;
-    };
-    // xhr.onprogress = function(event) {
-    //   progress.value = event.loaded / 1000000;
-    // };
+      // alert('Данные полностью загружены на сервер!');
+    }
+    xhr.upload.onerror = function(){
+      console.log('Произошла ошибка при загрузке данных на сервер!');
+    }
     xhr.send(formData);
     event.preventDefault();
   });
