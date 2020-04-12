b33b75b4f2cca6e33c7eae8141e4517bd9f1f1a9
Iva Kochneva <inkybasik@gmail.com>
2020-04-12 16:04:43 +0300

    Task 3 done

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -6,17 +6,15 @@ function poll() {
     const formData = new FormData(form); 
     const progress = document.getElementById('progress');
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php', true);
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php', true);
     xhr.upload.onprogress = function() {
-      // alert('Загружено на сервер');
       progress.value = 0.5;
     }
     xhr.upload.onload = function() {
       progress.value = 1;
-      // alert('Данные полностью загружены на сервер!');
     }
     xhr.upload.onerror = function(){
-      console.log('Произошла ошибка при загрузке данных на сервер!');
+      alert('Произошла ошибка при загрузке данных на сервер!');
     }
     xhr.send(formData);
     event.preventDefault();
